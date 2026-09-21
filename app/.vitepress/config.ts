78eb7642b05206d541318dbb75bdb3b79169392c
zjw <1723168479@qq.com>
2026-09-21 11:40:58 +0800

    fix: sitemap index合并入口sitemap地址

--- app/.vitepress/config.ts
+++ app/.vitepress/config.ts
@@ -274,6 +274,9 @@ export default defineConfig({
       sitemapIndex,
       `<?xml version="1.0" encoding="utf-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+   <sitemap>
+    <loc>${sitemapHostname}/sitemap.xml</loc>
+  </sitemap>
 ${branches
   .filter((br) => !br.includes('common'))
   .map((br) => {
